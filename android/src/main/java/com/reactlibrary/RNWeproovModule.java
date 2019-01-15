04dd4c0aad8b8cc76e53993267b2c2454ac670eb
Guillaume <guillaumeb90@gmail.com>
2019-01-15 15:54:32 +0100

    android first commit

--- android/src/main/java/com/reactlibrary/RNWeproovModule.java
+++ android/src/main/java/com/reactlibrary/RNWeproovModule.java
@@ -6,7 +6,15 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.widget.Toast;
+import report.Report;
+
+import com.weproov.sdk.WPLoadingActivity;
+import com.weproov.sdk.WPParameters;
+
 public class RNWeproovModule extends ReactContextBaseJavaModule {
+  
+  private static final int REQ_CODE = 2106;
 
   private final ReactApplicationContext reactContext;
 
@@ -19,4 +27,39 @@ public class RNWeproovModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNWeproov";
   }
+
+  @ReactMethod
+  public void setTheme(Callback success, Callback failure){
+    success.invoke("Success");
+  }
+
+  @ReactMethod
+  public void toast(String message){
+    Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_SHORT).show();
+  }
+
+  @ReactMethod
+  public void loadProovCode(String proovcode, Callback success, Callback failure){
+    WPParameters params = new WPParameters();
+    //params.isImportSectionVisibleByDefault = true;
+
+    getCurrentActivity().startActivityForResult(WPLoadingActivity.getIntent(getCurrentActivity(), proovcode, params), REQ_CODE);
+  }
+
+  @ReactMethod
+  public void loadTemplate(int templateId, Callback success, Callback failure){
+    //TODO
+  }
+
+  @ReactMethod
+  public void haveReportInCache(Callback success){
+    if(Report.getCurrent() != null){
+      success.invoke(1);
+    }
+    else{
+      success.invoke(0);
+    }
+    
+  }
+
 }
